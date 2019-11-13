aff6a78fd40e3e0d7f72170c3066d8250e78adab
lg-lg-lg <ls45t6ypi21shh@protonmail.com>
2019-11-13 19:51:01 +0500

    'filled_embedded_loops' stable test-snapshot

--- modules/tests.py
+++ modules/tests.py
@@ -1,5 +1,6 @@
 from argparse import ArgumentParser
 import random
+import functools
 
 from . import global_vars
 
@@ -11,6 +12,8 @@ MOCK_CLI_PASSWORD_STOP_POSITION = 3
 MOCK_CLI_OUTPUT_FOLDER = ""
 
 MOCK_checkup_last_word = True
+
+INFO_print_value = True
 INFO_print_main_variables = True
 INFO_password_generator = True
 
@@ -20,7 +23,8 @@ MAIN_VARS = global_vars.MainVariables()
 if MOCK_get_console_parameters:
     MAIN_VARS.PASSWORD_START_POSITION = MOCK_CLI_PASSWORD_START_POSITION
     MAIN_VARS.PASSWORD_STOP_POSITION = MOCK_CLI_PASSWORD_STOP_POSITION
-    MAIN_VARS.OUTPUT_FOLDER = MOCK_CLI_OUTPUT_FOLDER
+    if MOCK_CLI_OUTPUT_FOLDER:
+        MAIN_VARS.OUTPUT_FOLDER = MOCK_CLI_OUTPUT_FOLDER
 
 
 def get_console_parameters(func):
@@ -35,26 +39,56 @@ def get_console_parameters(func):
 
 def print_main_variables():
     if INFO_print_main_variables:
-        print(f"CHARSET = {MAIN_VARS.CHARSET}\n"
+        print(f"--- Test Mode is Enable. ---\n\n"
+              f"CHARSET = {MAIN_VARS.CHARSET}\n"
               f"PASSWORD_START_POSITION = {MAIN_VARS.PASSWORD_START_POSITION}\n"
               f"PASSWORD_STOP_POSITION = {MAIN_VARS.PASSWORD_STOP_POSITION}\n"
-              f"OUTPUT_FOLDER = {MAIN_VARS.OUTPUT_FOLDER if MAIN_VARS.OUTPUT_FOLDER else 'current'}")
+              f"OUTPUT_FOLDER = {MAIN_VARS.OUTPUT_FOLDER if MAIN_VARS.OUTPUT_FOLDER else 'current'}\n")
 
 
 def checkup_last_word(func):
-    if MOCK_checkup_last_word:
-        return "".join(random.choices(MAIN_VARS.CHARSET, k=MAIN_VARS.PASSWORD_STOP_POSITION))
-    return func
+
+    def tmp_result():
+        return lambda: "".join(random.choices(MAIN_VARS.CHARSET, k=MAIN_VARS.PASSWORD_STOP_POSITION))
+
+    # 'result_as_function' is a function
+    result_as_function = tmp_result() if MOCK_checkup_last_word else func()
+
+    if INFO_password_generator:
+        # print 'result_as_string' as a result of function 'result_as_function()'
+        result_as_string = result_as_function()
+        print_value(f"last_word = {result_as_string}")
+        return lambda: result_as_string  # string
+
+    return result_as_function  # function
 
 
 def password_generator(func):
-    # if INFO_password_generator:
-    # print(passgen.word)
-    # print(passgen.embedded_loops)
     return func
 
 
+def print_value(value):
+    if INFO_print_value:
+        print(value)
+
+
+def create_embedded_loops(func):
+    if INFO_password_generator:
+        result = func()
+        print_value(f"empty_embedded_loops = {result}")
+    return lambda: result
+
+
+def encode_word_to_char_positions_in_charset(func):
+    if INFO_password_generator:
+        @functools.wraps(func)
+        def wrapper(*args, **kwargs):
+            result = func(*args, **kwargs)
+            print_value(f"filled_embedded_loops = {result}")
+        return wrapper
+    return func
+
 # -------------------------------------
 
-print("--- Test Mode is Enable. ---\n")
+
 print_main_variables()
--- passgen.py
+++ passgen.py
@@ -1,7 +1,6 @@
 from argparse import ArgumentParser
 import sys
 import re
-import os.path
 
 from modules import global_vars, tests
 
@@ -19,14 +18,17 @@ def get_console_parameters() -> []:  # list
 
 @tests.checkup_last_word
 def checkup_last_word() -> str:
-    # checkup 'last_word' file and return last_word value
+    # checkup 'last_word' file and return 'last_word' value
     try:
         # 'last_word' file exist
-        with open(main_variables.OUTPUT_FOLDER + r"\last_word", "r") as file:
-            for line in file:
-                if line != "":
-                    last_word = re.sub(r"^\s+|\n|\r|\s+$", '', line)
-                    return last_word
+        with open(main_variables.OUTPUT_FOLDER + r"\last_word", "r") as last_word_file:
+            # last_word - is a single word in 'last_word' file, but it's last in generating chain
+            # last_word = word from 'last_word' file    or   'a'*3 -> 'aaa' (for example)
+            word_from_last_word_file = last_word_file.readline()
+            word_from_last_word_file = re.sub(r"^\s+|\n|\r|\s+$", '', word_from_last_word_file)
+            last_word: str = word_from_last_word_file if word_from_last_word_file \
+                else main_variables.CHARSET[0] * main_variables.PASSWORD_START_POSITION
+            return last_word
 
     except FileNotFoundError:
         # 'last_word' file not exist
@@ -39,26 +41,30 @@ def password_generator():
     # write every string into 'tmp_dict' file
     # return last generated string on error
 
-    with open(main_variables.OUTPUT_FOLDER + r"\tmp_dict", "a") as file:
-
-        # last_word - is first unwritten word in 'last_dict' file, but it's last in generating chain
-        # start word = word from 'last_word' file
-        # or 'a'*3 -> 'aaa' (for example)
-        word: str = last_word if last_word else main_variables.CHARSET[0] * main_variables.PASSWORD_START_POSITION
-
+    @tests.create_embedded_loops
+    def create_embedded_loops() -> dict:
         # forming embedded loops: word length = 7 -> 7 loops -> dict['char1':0, 'char2':0 .. 'char7':0]
         # where 0 - char position in CHARSET
-        embedded_loops: dict = {f"char{i+1}": 0 for i in range(main_variables.PASSWORD_STOP_POSITION)}
+        return {f"char{i+1}": 0 for i in range(main_variables.PASSWORD_STOP_POSITION)}
 
+    @tests.encode_word_to_char_positions_in_charset
+    def encode_word_to_char_positions_in_charset(word, loops) -> dict:
         # encode 'word' to char positions in CHARSET
         # 'aah8' -> dict['char1':0, 'char2':0, 'char3':8, 'char4':61]
-        for i, char in enumerate(embedded_loops):
-            embedded_loops[char] = main_variables.CHARSET.find(word[i])
+        for i, char in enumerate(loops):
+            loops[char] = main_variables.CHARSET.find(word[i])
+        return loops
+
+    with open(main_variables.OUTPUT_FOLDER + r"\tmp_dict", "a") as tmp_dict_file:
+
+        embedded_loops = create_embedded_loops()
+        last_word = checkup_last_word()
+        encode_word_to_char_positions_in_charset(last_word, embedded_loops)
 
         # while True:
-        #     if file.writable():
-        #         file.write(fr"{word}")  # for example 'aa' or 'v7-'
-        #         file.write("\r\n")
+        #     if tmp_dict_file.writable():
+        #         tmp_dict_file.write(fr"{word}")  # for example 'aa' or 'v7-'
+        #         tmp_dict_file.write("\r\n")
         #     else:
         #         return fr""
 
@@ -71,4 +77,4 @@ if __name__ == "__main__":
     cli_args = get_console_parameters()
     main_variables = global_vars.MainVariables(cli_args.start, cli_args.stop, cli_args.output)
 
-    # password_generator()
+    password_generator()
